f22744900291e354c1a799006accb5480183715d
Ashley <ashleywong678@gmail.com>
2021-04-29 19:32:52 -0400

    Create my_solution.py

---
+++ my_solution.py
@@ -0,0 +1,32 @@
+class ListNode:
+    def __init__(self, value = 0, next = None): 
+        self.value = value
+        self.next = next
+        
+def arrayify(head) -> [int]:
+    array = []
+    ptr = head
+    while ptr != None:
+        array.append(ptr.value)
+        ptr = ptr.next
+    return array
+
+def reverseLL(head: ListNode) -> ListNode:
+    res = None
+    prev = None
+    curr = head
+    next = head.next
+    while curr:
+        curr.next = prev
+        res = curr
+        prev = curr
+        curr = next
+        if next:
+            next = next.next
+    return res
+
+# Test Cases
+LL1 = ListNode(13, ListNode(1, ListNode(5, ListNode(3, ListNode(7, ListNode(10))))))
+print(arrayify(reverseLL(ListNode(1)))) # [1]
+print(arrayify(reverseLL(ListNode(1, ListNode(2))))) # [2, 1]
+print(arrayify(reverseLL(LL1))) # [10, 7, 3, 5, 1, 13]
